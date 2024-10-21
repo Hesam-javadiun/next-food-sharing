190b3940ff4df521b9849a172fae18251b632900
hesam javadiun <hesam.javadiun@gmail.com>
2024-10-21 05:28:25 -0700

    grid component added

---
+++ src/components/UI/grid/grid-container.tsx
@@ -0,0 +1,34 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
+
+type GridContainerProps<T extends ElementType> = {
+  as?: T;
+  children: ReactNode;
+} & ComponentPropsWithoutRef<T>;
+
+const GridContainer = function <tagType extends ElementType>(
+  props: GridContainerProps<tagType>
+) {
+  const { as, children, className, containerProps } = props;
+  const Component = as ?? 'ul'
+  return (
+    <Component
+      {...containerProps}
+      className={` ${className ?? ""}`}
+    >
+      {children}
+    </Component>
+  );
+};
+
+export default GridContainer;
+
+// .container {
+//   width: min(100%, 90rem);
+//   margin: auto;
+//   display: grid;
+//   grid-template-columns: repeat(auto-fit ,minmax( 20rem,1fr));
+//   gap: 5rem;
+//   grid-template-rows: auto ;
+//   overflow-y: auto;
+//   list-style: none;
+// }
